d6052439eb01109756c3a74d979032397cd54f66
truongtrinh1812 <htnewpro@gmail.com>
2023-04-18 14:27:47 +0700

    Add Duende Identity

---
+++ App/Identity/Config.cs
@@ -0,0 +1,41 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Identity;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope("api", new []{"name"}),
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+            {
+                new Client
+                {
+                    ClientId = "spa",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+
+                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                    BackChannelLogoutUri = "https://localhost:5002/bff/backchannel",
+
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "api" }
+                },
+            };
+    }
+}
