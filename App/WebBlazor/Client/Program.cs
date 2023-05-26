8e00a5981bc6bbf4e67e2d036a812e45947757c5
truongtrinh1812 <htnewpro@gmail.com>
2023-05-26 17:17:23 +0700

    Add RestEase

--- App/WebBlazor/Client/Program.cs
+++ App/WebBlazor/Client/Program.cs
@@ -2,7 +2,10 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebBlazor.Client;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using AppContracts.RestApi;
+using AppContracts;
+using RestEase;
+using RestEase.HttpClientFactory;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -21,7 +24,19 @@ builder.Services.AddHttpClient("backend", client => client.BaseAddress = new Uri
 builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("backend"));
 
 // builder.Services.AddTransient<CustomAuthorizationMessageHandler>();
-builder.Services.AddHttpClient<ProductClient>("gateway",
-        client => client.BaseAddress = new Uri("https://ocelotwebapigatewayapp:7050/"));
+var appName = AppConstants.ProductApiName;
+
+var appUri = $"http://{appName}:7003";
+
+builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
+builder.Services.AddRestEaseClient(typeof(AppContracts.RestApi.IProductApi), appUri, client =>
+{
+    client.RequestPathParamSerializer = new StringEnumRequestPathParamSerializer();
+}).AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+
+// builder.Services.AddRestClient<CustomAuthorizationMessageHandler>(
+//      typeof(IProductApi),
+//                 AppConstants.ProductApiName,
+//                 7003);
 
 await builder.Build().RunAsync();
